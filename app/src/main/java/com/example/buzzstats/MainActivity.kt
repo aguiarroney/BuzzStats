7a846e3be78835816ed814612ffac96b6618d2dd
aguiarroney <aguiarroney@gmail.com>
2019-11-24 18:10:30 -0200

    implementacao basica da funcionalidade de subtrair

--- app/src/main/java/com/example/buzzstats/MainActivity.kt
+++ app/src/main/java/com/example/buzzstats/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.buzzstats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,17 +11,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val plusBtn : Button = findViewById(R.id.plus_btn)
+        val plusFab : FloatingActionButton = findViewById(R.id.plus_fab)
+        val minusFab : FloatingActionButton = findViewById(R.id.minus_fab)
 
-        plusBtn.setOnClickListener {
-            somaContador()
+        plusFab.setOnClickListener {
+            modifyContador(1)
+        }
+
+        minusFab.setOnClickListener {
+            modifyContador(2)
         }
     }
 
-    private fun somaContador(){
-        val qntAtualEditText : TextView  = findViewById(R.id.contador)
-        val qntAtualInt = qntAtualEditText.text.toString().toInt()
-        val soma = qntAtualInt + 1
-        qntAtualEditText.text = soma.toString()
+    private fun modifyContador(operador : Int){
+        var soma: Int
+        val qntAtualTextView: TextView  = findViewById(R.id.contador)
+        var qntAtualText = qntAtualTextView.text
+
+        if(qntAtualText.isEmpty())
+            qntAtualText = "0"
+
+
+        println("[LOG: MainActivity] TextView Value: "+qntAtualText)
+
+
+        if(operador == 1){
+            soma = qntAtualText.toString().toInt() + 1
+        }
+
+        else if(operador == 2 && qntAtualText.toString().toInt()>0){
+            soma = qntAtualText.toString().toInt() - 1
+        }
+
+        else{
+            soma = qntAtualText.toString().toInt()
+        }
+
+        qntAtualTextView.text = soma.toString()
+
     }
 }
